1ad70de3168b615197c57abd3b0a07888a07ccd4
Alex Kravetz <alex@firsthandcares.com>
2025-03-16 14:24:25 -0600

    consts

--- src/ammoseekr/consts.py
+++ src/ammoseekr/consts.py
@@ -1,26 +1,17 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class Caliber(str, Enum):
+class Caliber(StrEnum):
     PISTOL_9MM = "9mm-luger"
     RIFLE_556_NATO = "5.56x45mm-nato"
     RIFLE_300_BO = "300aac-blackout"
 
-    def __str__(self):
-        return self.value
 
-
-class Casing(str, Enum):
+class Casing(StrEnum):
     BRASS = "brass"
     STEEL = "steel"
 
-    def __str__(self):
-        return self.value
 
-
-class Condition(str, Enum):
+class Condition(StrEnum):
     NEW = "new"
     USED = "user"
-
-    def __str__(self):
-        return self.value
